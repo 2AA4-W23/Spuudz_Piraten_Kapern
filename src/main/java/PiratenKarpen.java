f5f234e0c7f2ac7de5cf668c43b101b08a3a9237
Spuudz <bradbm1@mcmaster.ca>
2023-01-16 13:05:04 -0500

    Ends turn at 3 skulls, randomly keeps dice

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -4,10 +4,9 @@ public class PiratenKarpen {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
-        System.out.println("This is one roll!");
+        System.out.println("This is one turn!");
         Player p1 = new Player();
         p1.rollEight();
-        System.out.println(p1.getHand());
         System.out.println(p1.getScore());
 
         System.out.println("That's all folks!");
--- src/main/java/pk/Player.java
+++ src/main/java/pk/Player.java
@@ -1,5 +1,6 @@
 package pk;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Player {
     public int score;
@@ -9,6 +10,9 @@ public class Player {
     public Player(){
         score = 0;
         numSkull = 0;
+        for(int i = 0; i < 8;i++){
+            currentHand.add("PLACEHOLDER");
+        }
     }
     public int getScore(){
         return score;
@@ -20,13 +24,47 @@ public class Player {
 
     public void rollEight(){
         ArrayList<String> randomHand = new ArrayList<String>();
+        randomHand = currentHand;
         Dice myDice = new Dice();
-        for (int i = 0; i < 8; i++){
-            String myRoll = myDice.roll().toString();
-            randomHand.add(myRoll);
+        while(numSkull < 3){
+            for (int i = 0; i < (8-numSkull); i++){
+                String myRoll = myDice.roll().toString();
+                randomHand.set(i,myRoll);
+            }
+            System.out.println(randomHand);
+            removeSkulls(randomHand);
+            System.out.println(randomHand);
+            reroll(randomHand,myDice,numSkull);
+            System.out.println(randomHand);
+            removeSkulls(randomHand);
+            System.out.println(randomHand);
+            System.out.println("------------------");
         }
         currentHand = randomHand;
         updateScore();
+    }
+    public void removeSkulls(ArrayList<String> randomHand){
+        for(int i = 0; i < randomHand.size(); i++){
+            if(randomHand.get(i) == "SKULL"){
+                randomHand.remove(String.valueOf("SKULL"));
+                numSkull++;
+                i--;
+                System.out.println("DEBUG found a skull");
+            }
+        }
+    }
+    public void reroll(ArrayList<String> randomHand, Dice myDice,int numSkull){
+        if(numSkull < 3){
+            Random rand = new Random();
+
+            for(int i = 0; i < randomHand.size();i++){
+                int changeRoll = rand.nextInt(2);
+
+                if(changeRoll == 1){
+                    randomHand.set(i,myDice.roll().toString());
+                }
+            }
+        }
     }
 
     public void updateScore(){
@@ -36,7 +74,7 @@ public class Player {
                 count++;
             }
         }
-        score+=(count*100);
+        score+=(count*100); 
     }
 
 
