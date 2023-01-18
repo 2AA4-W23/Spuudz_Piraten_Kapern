f648699ec712cebe51b3287fa46c9e780bf0da3c
Spuudz <bradbm1@mcmaster.ca>
2023-01-18 16:25:03 -0500

    *num* of a kind score system completed

--- src/main/java/pk/Player.java
+++ src/main/java/pk/Player.java
@@ -60,25 +60,58 @@ public class Player {
     public void reroll(ArrayList<String> randomHand, Dice myDice,int numSkull){
         if(numSkull < 3){
             Random rand = new Random();
+            int changeRoll = rand.nextInt(randomHand.size()+1);
 
-            for(int i = 0; i < randomHand.size();i++){
-                int changeRoll = rand.nextInt(2);
-
-                if(changeRoll == 1){
-                    randomHand.set(i,myDice.roll().toString());
-                }
+            for(int j = 0; j < changeRoll;j++){
+                randomHand.set(j,myDice.roll().toString());
             }
         }
     }
 
     public void updateScore(){
+        logger.debug("Previous score: " + score);
+        ArrayList<Integer> combo = new ArrayList<Integer>(); 
         int count = 0;
+        int stack = 0;
+        int temp = 0;
+        int max = 0;
+
         for(int i = 0; i < currentHand.size(); i++){
+            temp=0;
             if (currentHand.get(i) == "DIAMOND" || currentHand.get(i) == "GOLD"){
                 count++;
             }
+            for(int j = i; j < currentHand.size(); j++){
+                if (currentHand.get(i) == currentHand.get(j))
+                    temp++;
+            }
+            if(temp > max)
+                max = temp;
+            combo.add(temp);
         }
+
+        for(int k = 0; k < combo.size(); k++){
+            if(combo.get(k) == max)
+                stack++;
+        }
+
+        for(int a = 0; a < stack; a++){
+            if (max==3)
+                score+=100;
+            else if(max==4)
+                score+=200;
+            else if(max==5)
+                score+=500;
+            else if(max==6)
+                score+=1000;
+            else if(max==7)
+                score+=2000;
+            else if(max==8)
+                score+=4000;
+        }
+
         score+=(count*100); 
+        logger.debug(max + ":combo | stack:" + stack);
         logger.debug(score);
         logger.debug("------------------");
     }
